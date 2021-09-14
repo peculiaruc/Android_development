3e7554425ea07d8db8f6e642a280d12b342cb41a
Espérant GADA <esperantgada@gmail.com>
2021-09-14 08:10:26 +0100

    Want to fix an error

---
+++ app/src/main/java/com/example/boa/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.boa
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.boa.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding =
+            DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        drawerLayout = binding.drawerLayout
+
+
+        //Adding up button
+        val navController = this.findNavController(R.id.myNavHostFragment)
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
+    }
+
+
+    //method for up button
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+}
